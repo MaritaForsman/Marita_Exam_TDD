d6c7745a24eaae4926c8dbfb57ce277a68a8b814
Marita Forsman <maritaforsman57@gmail.com>
2016-02-18 09:20:19 +0100

    Added first testcase

---
+++ UnitTestExam/Exam/Classes/Invånare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Classes
+{
+    public class Invånare
+    {
+        public object AntalInvånare(object p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object antalår(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTestExam/UnitTestExam/UnitTestuppgift1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exam.Classes;
+
+namespace UnitTestExam
+{
+    [TestClass]
+    public class UnitTestuppgift1
+    {
+        [TestMethod]
+        public void TestAntalInvånare()
+        {
+            Invånare i = new Invånare();
+            var inv = i.AntalInvånare(i.antalår(3));
+            Assert.AreEqual(25875, inv);
+
+        }
+
+        
+    }
+}
